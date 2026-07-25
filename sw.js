b16f8db962cb4f115f9ede0706868ee0e00ab71b
JETECH <jetechapp@gmail.com>
2026-07-25 08:48:37 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+// sw.js
+const CACHE_NAME = 'qotia-work-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/css/main.css', // se dividi i file, altrimenti tutto è in index
+  '/manifest.json',
+  '/assets/logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
